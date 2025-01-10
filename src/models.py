ada2fdcaa11bcc790f4efaab7a14504e2ba8fb85
luciacanalda <luciacanaldas@gmail.com>
2025-01-10 14:01:43 +0000

    Finished Project

--- src/models.py
+++ src/models.py
@@ -7,12 +7,14 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
+class User(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(50), nullable=False)
+    lastname = Column (String(50), nullable=False)
+    email = Column (String(50), nullable=False)
 
 class Address(Base):
     __tablename__ = 'address'
@@ -22,11 +24,54 @@ class Address(Base):
     street_name = Column(String(250))
     street_number = Column(String(250))
     post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    person_id = Column(Integer, ForeignKey('user.id'))
+    person = relationship(User)
+
+class Post (Base):
+  __tablename__ = 'post'
+  id = Column(Integer, primary_key=True)
+  user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+  image_url = Column(String(255), nullable=False)
+  caption = Column(String(500))
+  location = Column(String(100))
+
+  user = relationship('User', back_populates='posts')
+  comments = relationship('Comment', back_populates='post', cascade='all, delete')
+  likes = relationship('Like', back_populates='post', cascade='all, delete')
+  
+  class Comment(Base):
+    __tablename__ = 'comment'
+    
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    content = Column(String(500), nullable=False)
+    
+
+    user = relationship ("User")
+    post = relationship ("Post", back_populates="comments")
+
+    class Like(Base):
+        
+        __tablename__ = 'like'
+    
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    
+
+    user = relationship ("User")
+    post = relationship ("Post", back_populates= "likes")
 
     def to_dict(self):
-        return {}
+        return { 
+            'id': self.id,
+            'post_id': self.post_id,
+            'user_id': self.user_id,
+        
+
+        }
+    
 
 ## Draw from SQLAlchemy base
 try:
